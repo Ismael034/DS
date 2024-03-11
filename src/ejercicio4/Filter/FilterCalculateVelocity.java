35a5a02a9562ee39f579a7839b2aa0c0c9467ad8
augustote <augustogarmo@gmail.com>
2024-03-11 16:47:54 +0100

    first-commit

---
+++ src/ejercicio4/Filter/FilterCalculateVelocity.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio4.Filter;
+
+import ejercicio4.MotorState.MotorState;
+
+/**
+ *
+ * @author augus
+ */
+public class FilterCalculateVelocity implements Filter {
+    @Override
+    public double ejecutar(double revoluciones, MotorState estadoMotor) {
+        switch(estadoMotor) {
+            case MotorState.ACELERANDO:
+                revoluciones += INCREASEVELOCITY;
+                if (revoluciones > MAXVELOCITY) {
+                    revoluciones = MAXVELOCITY;
+                }
+                break;
+            case MotorState.FRENANDO:
+                revoluciones -= INCREASEVELOCITY;
+                if (revoluciones < 0) {
+                    revoluciones = 0;
+                }
+                break;
+        }
+        
+        return revoluciones;
+    }
+}
