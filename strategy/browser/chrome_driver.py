961934b0e7e7f08446edb8350c6d0d3b4939233a
Ismael <ismael034@correo.ugr.es>
2024-03-18 00:06:37 +0100

    Scrapper terminado

--- strategy/browser/chrome_driver.py
+++ strategy/browser/chrome_driver.py
@@ -1,18 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from strategy.browser.browser_strategy import BrowserStrategy
 
 class ChromeDriver(BrowserStrategy):
-    def __init__(self, config):
-        self.config = config
+    def __init__(self):
         self.service = Service(ChromeDriverManager().install())
         self.options = Options()
         self.options.add_argument("--headless")
         self.options.add_argument("--start-maximized")
-        self.wait = WebDriverWait
 
     def get_driver(self):
         return webdriver.Chrome(service=self.service, options=self.options)
--- strategy/browser/firefox_driver.py
+++ strategy/browser/firefox_driver.py
@@ -3,17 +3,14 @@ from selenium.webdriver import FirefoxOptions
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
 from strategy.browser.browser_strategy import BrowserStrategy
-from selenium.webdriver.support.wait import WebDriverWait
 
 class FirefoxDriver(BrowserStrategy):
-    def __init__(self, config):
-        self.config = config
+    def __init__(self):
         self.service = Service(GeckoDriverManager().install())
         self.options = FirefoxOptions()
         self.options.add_argument("--headless")
         self.options.add_argument("--start-maximized")
         self.options.add_argument("--kiosk")
-        self.wait = WebDriverWait
 
     def get_driver(self):
         return webdriver.Firefox(service=self.service, options=self.options)
