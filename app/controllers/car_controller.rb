31160652eda15bcfd6c8258ae47a8d30796ea2cf
Ismael <42477828+Ismael034@users.noreply.github.com>
2024-05-20 16:22:55 +0200

    Return car data on index

--- app/controllers/car_controller.rb
+++ app/controllers/car_controller.rb
@@ -11,10 +11,12 @@ class CarController < ApplicationController
 
   def index
     @cars = Car.all
+    render json: @cars
   end
 
   def show
     @car = Car.find(params[:id])
+    render json: @car
   end
 
   def update
