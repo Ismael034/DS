9beeab1c5e1bd87be87d6df8161dacc1a475be54
erpiedra <pedrogris@correo.ugr.es>
2024-05-24 17:07:01 +0200

    car_test arreglado

--- test/car_test.dart
+++ test/car_test.dart
@@ -1,23 +1,19 @@
+import 'package:concesionario_tunning/user/user.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:concesionario_tunning/car_facade.dart';
 import 'package:concesionario_tunning/builder/coche.dart';
-import 'package:concesionario_tunning/user/user.dart';
-import 'package:concesionario_tunning/api_client.dart';
 
 void main() {
   group('CarFacade', () {
     late CarFacade facade;
-    late ApiClient apiClient;
-    late Map<String, dynamic> carData;
+    late dynamic carData;
     late double capacidad;
     late double gastoKm;
     late User testUser;
-    List<Coche> allCoches = [];
 
     setUp(() async {
       facade = CarFacade();
       testUser = User(id: 7, name: "Test");
-      apiClient = ApiClient();
       capacidad = 100.0;
       gastoKm = 10.0;
       carData = {
@@ -28,7 +24,7 @@ void main() {
       };
 
       await facade.setUser(testUser);
-      allCoches = facade.getCars();
+      final allCoches = facade.getCars();
       if (allCoches.isNotEmpty) {
         int i = allCoches.length - 1;
         while (i >= 0) {
@@ -36,59 +32,47 @@ void main() {
           i--;
         }
       }
-
-      await facade.buildCar(carData);
     });
-
     test('Gasoline vehicle creation test', () async {
-      allCoches = await apiClient.getCochesUsuario(facade.getUser().id);
-      facade.setCars(allCoches);
       carData['tipoCombustible'] = 'Gasolina';
       await facade.buildCar(carData);
-      allCoches = facade.getCars();
-      expect(allCoches.first.tipoCombustible, Combustible.gas);
-      expect(allCoches.first.autonomia, capacidad * gastoKm);
-      expect(allCoches.first.tiempoRecarga, 0);
-      expect(allCoches.first.costeRecarga, capacidad * 1.2);
+      expect(facade.getCars().first.tipoCombustible, Combustible.gas);
+      expect(facade.getCars().first.autonomia, capacidad * gastoKm);
+      expect(facade.getCars().first.tiempoRecarga, 0);
+      expect(facade.getCars().first.costeRecarga, capacidad * 1.2);
     });
 
     test('Hybrid vehicle creation test', () async {
-      allCoches = await apiClient.getCochesUsuario(facade.getUser().id);
-      facade.setCars(allCoches);
       carData['tipoCombustible'] = 'Hibrido';
       await facade.buildCar(carData);
-      allCoches = facade.getCars();
-      expect(allCoches.first.tipoCombustible, Combustible.hib);
-      expect(allCoches.first.autonomia, (capacidad * gastoKm) * 1.1);
-      expect(allCoches.first.tiempoRecarga, capacidad / 3);
-      expect(allCoches.first.costeRecarga, capacidad * 1.2);
+      expect(facade.getCars().last.tipoCombustible, Combustible.hib);
+      expect(facade.getCars().last.autonomia, (capacidad * gastoKm) * 1.1);
+      expect(facade.getCars().last.tiempoRecarga, 33.33333333333333);
+      expect(facade.getCars().last.costeRecarga, capacidad * 1.2);
     });
 
     test('Electric vehicle creation test', () async {
-      allCoches = await apiClient.getCochesUsuario(facade.getUser().id);
-      facade.setCars(allCoches);
       carData['tipoCombustible'] = 'Electrico';
       await facade.buildCar(carData);
-      allCoches = facade.getCars();
-      expect(allCoches.first.tipoCombustible, Combustible.elec);
-      expect(allCoches.first.autonomia, capacidad * gastoKm);
-      expect(allCoches.first.tiempoRecarga, capacidad / 3);
-      expect(allCoches.first.costeRecarga, capacidad * 0.1);
+      expect(facade.getCars().last.tipoCombustible, Combustible.elec);
+      expect(facade.getCars().last.autonomia, capacidad * gastoKm);
+      expect(facade.getCars().last.tiempoRecarga, 33.33333333333333);
+      expect(facade.getCars().last.costeRecarga, capacidad * 0.1);
     });
 
     test('Wrong fuel type test', () async {
       carData['tipoCombustible'] = 'Wrong';
-      expect(() => facade.buildCar(carData), throwsException);
+      expect(() async => await facade.buildCar(carData), throwsException);
     });
 
     test('Incorrect capacity value', () async {
       carData['capacidad'] = -1;
-      expect(() => facade.buildCar(carData), throwsException);
+      expect(() async => await facade.buildCar(carData), throwsException);
     });
 
     test('Incorrect consumption value', () async {
       carData['gastoKm'] = -1;
-      expect(() => facade.buildCar(carData), throwsException);
+      expect(() async => await facade.buildCar(carData), throwsException);
     });
   });
 }
